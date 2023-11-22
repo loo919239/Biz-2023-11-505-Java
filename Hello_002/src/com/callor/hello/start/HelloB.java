92f12bda67dc2c95b2a3b126dd177781f2a3dd84
loo919239 <loo919239@919239.com>
2023-11-22 13:24:42 +0900

    시작

---
+++ Hello_002/src/com/callor/hello/start/HelloB.java
@@ -0,0 +1,27 @@
+package com.callor.hello.start;
+
+public class HelloB {
+	public static void main(String[] args) {
+		/*
+		 * num1 이라는 보관소를 예약하고 정수 30을 보관
+		 * 단, num1 변수(보관소)에는 int type 의 정수만
+		 * 보관하겠다. long type 의 정수는 보관하지 않겠다.
+		 * num2 라는 보관서를 예약하고 정수 40을 보관
+		 * 단, num2 변수에는 int type 의 정수만 보관하겠다.
+		 */
+		int num1 = 30;
+		int num2 = 40;
+		
+		/*
+		 * long type num3, num4 의 변수를 예약하고
+		 * 각각 정수 30과 40을 보관하겠다.
+		 * 
+		 */
+		
+		long num3 = 30;
+		long num4 = 40;
+		
+
+	}
+
+}
---
+++ Hello_002/src/com/callor/hello/ver/VerA.java
@@ -0,0 +1,40 @@
+package com.callor.hello.ver;
+
+public class VerA {
+	public static void main(String[] args) {
+		// 정수형변수 num1을 선언하고 0으로 clear
+		int num1 = 0;
+		int num2 = 0;
+		
+		int sum = 0;
+		
+		/*
+		 * 변수 만들기
+		 * 1. 변수에 어떤 데이터를 보관할 것인가 정하기
+		 * 2. 변수에 보괄할 데이터가 어떤 type인가 살펴보기
+		 * 3. 보관할 데이터 type의 키워드 확인하기(int, long)
+		 * 4. 변수 이름 짓기
+		 * 	가. 가급력 2개 이상의 단어를 조합하여
+		 * 		변수에 어떤 데이터가 담기고, 어떤 일에 쓰일지
+		 * 		이름만 보고 알수 있도록 만든다
+		 * 	나. 첫글자는 반드시 영문소문자
+		 * 	다. 2개 이상의 단어를 조합할때는 
+		 * 		두번째 부터 각 단어 첫글자는 대문자,
+		 * 		나머지는 소문자
+		 * 		(Camel case 라고 한다)
+		 * 	라. 비슷한 유형의 변수가 있을때는 영문자+숫자 조합
+		 */
+		
+		int inKor = 0;
+		int intEng = 0;
+		int inTotal = 0;
+		
+		int scoreKor = 0;
+		int scoreEng = 0;
+		int scoreMath = 0;
+		int scoreMusic = 0;
+		int scoreArt = 0;
+		
+	}
+
+}
