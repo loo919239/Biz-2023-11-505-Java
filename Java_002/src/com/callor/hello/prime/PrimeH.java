c27e9962c79e1a2ddda745bf8404a697f94f10c5
loo919239 <loo919239@loo919239.com>
2023-11-27 19:47:46 +0900

    과제

--- Java_002/src/com/callor/hello/prime/PrimeH.java
+++ Java_002/src/com/callor/hello/prime/PrimeH.java
@@ -1,24 +1,28 @@
 package com.callor.hello.prime;
 
-public class PrimeG {
+public class PrimeH {
 	public static void main(String[] args) {
+		System.out.println("2~100까지 수 중 소수들의 합");
+
 //		int num = 0;
 		int index = 0;
-		System.out.println("==================================");
-		System.out.println("소수 판별하기");
-		System.out.println("----------------------------------");
+		int sum = 0;
+
 		for (int i = 1; i < 100; i++) {
-			int num = i+1;
+			int num = i + 1;
 			for (index = 2; index < num + 1; index++) {
 				if (num % index == 0) {
 					break;
 				}
 			}
 			if (index < num) {
+
 			} else {
-				System.out.println(num + "는 소수임");
+				sum += num;
 			}
 		}
+		System.out.println(sum);
 		
+		// sum += i + 1;
 	}
 }
