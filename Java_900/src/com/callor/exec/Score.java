3af450809bee985d08e4915c6c16eb61850c09dd
loo919239 <loo919239@naver.com>
2023-12-08 13:30:33 +0900

    15일차

--- Java_900/src/com/callor/exec/Score.java
+++ Java_900/src/com/callor/exec/Score.java
@@ -1,17 +1,11 @@
-package com.callor.score;
+package com.callor.exec;
 
 import com.callor.score.service.ScoreService;
-import com.callor.score.utils.Line;
 
 public class Score {
-
 	public static void main(String[] args) {
 		ScoreService scoreService = new ScoreService();
-
 		scoreService.inputScore();
-
 		scoreService.printScore();
-
 	}
-
 }
